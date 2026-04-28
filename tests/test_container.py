f5f70381d512b403ea2bce406f7249d4167259f0
Zachariah314 <zloewen314@gmail.com>
2026-04-28 13:30:21 -0700

    added test file

---
+++ tests/test_container.py
@@ -0,0 +1,26 @@
+import pytest
+from src.di_container import Container, Inject
+
+# -------------------------
+# Test classes
+# -------------------------
+class A:
+    pass
+
+class B:
+    def __init__(self, a: A):
+        self.a = a
+
+# -------------------------
+# Basic resolution
+# -------------------------
+def test_simple_resolution():
+    c = Container()
+    c.register(A, A)
+
+    b = c.resolve(B)
+
+    assert isinstance(b, B)
+    assert isinstance(b.a, A)
+
+
